3b5b7a626748887bc63fa83aab684b36c3443838
dydwo6018 <dydwo6018@gmail.com>
2025-05-12 20:13:37 +0900

    feat: 로그인 성공 시 토큰 반환용 TokenResponse DTO 구현

---
+++ src/main/java/com/springbootjwtauth/dto/response/TokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.springbootjwtauth.dto.response;
+
+public class TokenResponseDto {
+    private String token;
+
+    public TokenResponseDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
