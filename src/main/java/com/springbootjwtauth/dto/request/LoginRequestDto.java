5dfa5093e5a69a2b6441a1dac448acbbadfc70cc
dydwo6018 <dydwo6018@gmail.com>
2025-05-13 19:07:47 +0900

    refactor : 어노테이션 추가

--- src/main/java/com/springbootjwtauth/dto/request/LoginRequestDto.java
+++ src/main/java/com/springbootjwtauth/dto/request/LoginRequestDto.java
@@ -1,8 +1,12 @@
 package com.springbootjwtauth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     private String username;
