9ac7a8604fe37afee379d412efe57069dc1f6dd7
dydwo6018 <dydwo6018@gmail.com>
2025-05-14 23:56:40 +0900

    docs: AuthController에 Swagger 문서화 어노테이션 추가

--- src/main/java/com/springbootjwtauth/controller/AuthController.java
+++ src/main/java/com/springbootjwtauth/controller/AuthController.java
@@ -9,6 +9,9 @@ import com.springbootjwtauth.exception.ErrorCode;
 import com.springbootjwtauth.model.User;
 import com.springbootjwtauth.service.AuthService;
 import com.springbootjwtauth.store.UserStore;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Auth", description = "인증 및 권한 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
@@ -25,6 +29,7 @@ public class AuthController {
     private final UserStore userStore;
 
     // 회원가입 API
+    @Operation(summary = "회원가입", description = "회원가입 API 입니다.")
     @PostMapping("/signup")
     public ResponseEntity<UserResponseDto> signup(@RequestBody @Valid SignupRequestDto request) {
         UserResponseDto response = authService.signup(request);
@@ -32,6 +37,7 @@ public class AuthController {
     }
 
     // 로그인 API
+    @Operation(summary = "로그인", description = "로그인 API 입니다.")
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> login(@RequestBody @Valid LoginRequestDto request) {
         TokenResponseDto responseDto = authService.login(request);
@@ -39,6 +45,11 @@ public class AuthController {
     }
 
     // 관리자 권한 부여 API
+    @Operation(
+            summary = "관리자 권한 부여",
+            description = "해당 유저에게 관리자 권한을 부여하는 API 입니다.(ADMIN 전용)",
+            security = { @SecurityRequirement(name = "JWT Auth") }
+    )
     @PatchMapping("/admin/users/{username}/roles")
     @PreAuthorize("hasRole('ADMIN')") // 관리자만 호출 가능
     public ResponseEntity<UserResponseDto> grantAdminRole(@PathVariable String username) {
@@ -47,6 +58,10 @@ public class AuthController {
     }
 
     // 권한체크를 위한 API
+    @Operation(summary = "내 정보 조회",
+            description = "로그인한 사용자의 정보를 조회하는 API 입니다.",
+            security = { @SecurityRequirement(name = "JWT Auth") }
+    )
     @GetMapping("/me")
     public UserResponseDto getMyInfo(@AuthenticationPrincipal String username) {
         User user = userStore.findByUsername(username);
