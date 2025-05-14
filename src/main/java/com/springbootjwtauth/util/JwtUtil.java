d7648b3e048a68c76297129053fee424d5019480
dydwo6018 <dydwo6018@gmail.com>
2025-05-14 17:37:57 +0900

    refactor: JwtUtil을 username 기반으로 리팩토링

--- src/main/java/com/springbootjwtauth/util/JwtUtil.java
+++ src/main/java/com/springbootjwtauth/util/JwtUtil.java
@@ -1,13 +1,18 @@
 package com.springbootjwtauth.util;
 
-import io.jsonwebtoken.*;
+import com.springbootjwtauth.exception.CustomException;
+import com.springbootjwtauth.exception.ErrorCode;
+import com.springbootjwtauth.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.UUID;
 
 @Component
 public class JwtUtil {
@@ -24,12 +29,13 @@ public class JwtUtil {
     }
 
     // JWT 토큰 생성 메서드
-    public String createAccessToken(UUID userId) {
+    public String createAccessToken(User user) {
         Date now = new Date();
         Date expiry = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
 
         return Jwts.builder()
-                .setSubject(userId.toString()) // 토큰에 사용자 ID 넣기
+                .setSubject(user.getUsername()) //username 기반
+                .claim("roles", user.getRoles().stream().map(Enum::name).toList())
                 .setIssuedAt(now)
                 .setExpiration(expiry)
                 .signWith(key, SignatureAlgorithm.HS256) // 키로 서명
@@ -37,26 +43,25 @@ public class JwtUtil {
     }
 
     // JWT 토큰에서 사용자 ID 꺼내기
-    public UUID getUserIdFromToken(String token) {
+    public String getUsernameFromToken(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
 
-        return UUID.fromString(claims.getSubject());
+        return claims.getSubject();
     }
 
     // JWT 토큰이 유효한지 확인하기
-    public boolean validateToken(String token) {
+    public void validateToken(String token) {
         try {
             Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token);
-            return true;
         } catch (JwtException | IllegalArgumentException e) {
-            return false;
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
         }
     }
 }
